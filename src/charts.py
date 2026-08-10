0477d96adcafd5279867012e6bd6b825cb36759f
노희주 <https://github.com/hiju200208-png/ai-data-analysis.git>
2026-08-10 09:51:00 +0900

    결과해석 추가

--- src/charts.py
+++ src/charts.py
@@ -5,11 +5,7 @@ def show_category_sales_chart(category_sales):
     st.bar_chart(
         category_sales,
         x="category",
-<<<<<<< HEAD
         y="total_sales",
-=======
-        y="total_sales"
->>>>>>> b3b7c3310d0822a9bc09cd825c66a752ba51c617
     )
 
 
@@ -17,9 +13,5 @@ def show_monthly_sales_chart(monthly_sales):
     st.line_chart(
         monthly_sales,
         x="order_month",
-<<<<<<< HEAD
         y="total_sales",
-=======
-        y="total_sales"
->>>>>>> b3b7c3310d0822a9bc09cd825c66a752ba51c617
     )
